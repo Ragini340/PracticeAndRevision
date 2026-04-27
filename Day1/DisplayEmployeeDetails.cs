46631c546ec873633d0603669a4136bde7575aca
Ragini340 <ragini31bxr@gmail.com>
2026-04-27 10:30:00 +0530

    #Practice Day 1

---
+++ Day1/DisplayEmployeeDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Day1
+{
+    public class DisplayEmployeeDetails
+    {
+        public void DisplayEmployee()
+        {
+            int empId = 101;
+            string empName = "John";
+            double salary = 50000;
+
+            Console.WriteLine("Employee Id : " + empId);
+            Console.WriteLine("Employee Name : " + empName);
+            Console.WriteLine("Salary : " + salary);
+        }
+
+        public static void Main(string[] args)
+        {
+            DisplayEmployeeDetails emp = new DisplayEmployeeDetails();
+            emp.DisplayEmployee();
+        }
+    }
+}
---
+++ Day1/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Day1
+{
+    public class HelloWorld
+    {
+        public void Display()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public static void Main(string[] args)
+        {
+            HelloWorld obj = new HelloWorld();
+            obj.Display();
+        }
+    }
+}
