de566146035390276bb37e6d67a1765cd48c9c0b
Ragini340 <ragini31bxr@gmail.com>
2026-05-06 10:30:00 +0530

    #Practice Added public and internal class Example

---
+++ Classes_And_Objects/TypesOfClasses/Internal/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Classes_And_Objects.TypesOfClasses.Internal
+{
+    internal class Employee
+    {
+        public void Display()
+        {
+            Console.WriteLine("Internal Class Accessed Successfully");
+        }
+
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Display();
+        }
+    }
+}
---
+++ Classes_And_Objects/TypesOfClasses/Public/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Classes_And_Objects.TypesOfClasses.Public
+{
+    public class Employee
+    {
+        public void Display()
+        {
+            Console.WriteLine("Public Class Accessed Successfully");
+        }
+
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Display();
+        }
+    }
+}
