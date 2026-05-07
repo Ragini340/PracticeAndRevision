01fc8c5bed379e122ee9b2e6445b0ff559364076
Ragini340 <ragini31bxr@gmail.com>
2026-05-07 19:25:27 +0530

    Add project files.

---
+++ Pattern_Printing/MirrorDiamondPattern.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Pattern_Printing
+{
+    /*Write a C# program to print a mirror diamond star (*) pattern based on the number of rows entered by the user.
+
+The program should:
+
+Accept the number of rows from the user
+Print the upper pyramid and lower inverted pyramid
+Use separate methods for:
+Reading input
+Printing spaces
+Printing stars
+Printing the upper half
+Printing the lower half
+  
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+    
+     */
+    public class MirrorDiamondPattern
+    {
+    }
+}
