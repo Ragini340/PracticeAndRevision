ebf884f4c77aa9134dedc2694bd2e8c9b1ad4b4d
Ragini340 <ragini31bxr@gmail.com>
2026-05-15 10:30:00 +0530

    #Practice Destructor POC

---
+++ Destructor/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Destructor
+{
+    public class Student
+    {
+        public Student()
+        {
+            Console.WriteLine("Constructor: Student object created");
+        }
+
+        ~Student()
+        {
+            Console.WriteLine("Destructor: Student object destroyed");
+        }
+
+        public static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Console.WriteLine("Main Method Completed");
+        }
+
+    }
+}
