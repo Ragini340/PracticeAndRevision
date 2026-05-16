a1ff748373a50b68066d7f6341b09e7f899bd14c
Ragini340 <ragini31bxr@gmail.com>
2026-05-16 10:30:00 +0530

    #Practice Static Constructor POC

---
+++ Constructors/Static_Constructor/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Constructors.Static_Constructor
+{
+    public class Student
+    {
+        static string College;
+
+        static Student()
+        {
+            College = "ABC College";
+        }
+    }
+}
