447daffa835274ad868ae31f0c916eed96f8ade6
Ragini340 <ragini31bxr@gmail.com>
2026-05-22 07:56:49 +0530

    #Static Constructor POC

---
+++ Constructor/Driver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PracticeAndRevision.Destructor;
+
+namespace PracticeAndRevision.Constructor
+{
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            StaticConstructor obj = new StaticConstructor(101, "John", 22);
+            StaticConstructor obj1 = new StaticConstructor(102, "RAM", 26);
+            StaticConstructor.DisplayCollege();
+            obj.DisplayStudent();
+            obj.DisplayStudent();
+        }
+    }
+}
