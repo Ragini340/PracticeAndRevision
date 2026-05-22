c512ac6e97e661032656ad32cbfb12c633657749
Ragini340 <ragini31bxr@gmail.com>
2026-05-22 07:50:17 +0530

    #Destructor Practice

---
+++ Destructor/Driver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Destructor
+{
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            Student stu = new Student();
+            Console.WriteLine("Main method execution completed");
+        }
+    }
+}
---
+++ Destructor/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Destructor
+{
+    public class Student
+    {
+        public Student()
+        {
+            Console.WriteLine("Constructor Student object created");
+        }
+        ~Student()
+        {
+            Console.WriteLine("Destructor Student object destroyed");
+        }
+    }
+}
