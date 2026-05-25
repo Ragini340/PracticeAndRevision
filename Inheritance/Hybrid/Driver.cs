c67bfb1e96cc056d2924078c68158737c5e9734a
Ragini340 <ragini31bxr@gmail.com>
2026-05-25 22:03:52 +0530

    #POC of Hybrid Inheritance

---
+++ Inheritance/Hybrid/Driver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Inheritance.Hybrid
+{
+    /*Hybrid Inheritance:
+    Hybrid is a combination of two types of inheritance that is single as well as Hierarchical inheritance.
+    */
+    internal class Driver
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.Study();
+
+            s.Display();
+            Teacher t = new Teacher();
+            t.Teach();
+            t.Display();
+        }
+    }
+}
---
+++ Inheritance/Hybrid/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Inheritance.Hybrid
+{
+    class Student : Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("Student is studying");
+        }
+    }
+}
