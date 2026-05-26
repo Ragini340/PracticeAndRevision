e3d9aea156f5a7bf2917f66c9656b7bb9d6bf944
Ragini340 <ragini31bxr@gmail.com>
2026-05-26 17:44:04 +0530

    #Default Constructor POC

---
+++ Constructor/Default_Constructor/Driver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Constructor.Default_Constructor
+{
+    internal class Driver
+    {
+        static void Main(string[] args)
+        {
+            Student objstu = new Student();
+            objstu.DisplayStudent();
+            Student obj = new Student();
+            obj.DisplayStudent();
+        }
+    }
+}
---
+++ Constructor/Default_Constructor/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Constructor.Default_Constructor
+{
+    class Student
+    {
+        //UserDefinedDefaultConstructor: it a paramter less constructor.
+        int sid;
+        string sname;
+        double sfee;
+        public Student()
+        {
+            sid = 1;
+            sname = "ABC";
+            sfee = 25000;
+        }
+        public void DisplayStudent()
+        {
+            Console.WriteLine("Student id is " + sid);
+
+            Console.WriteLine("Student name is " + sname);
+            Console.WriteLine("Student fee is " + sfee);
+        }
+    }
+}
