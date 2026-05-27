6cd01f6cdfcb31504446fdd890a39ea3b74bfebc
Ragini340 <ragini31bxr@gmail.com>
2026-05-27 21:45:21 +0530

    #Access Specifier POC

---
+++ Access_Modifier/Internal/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Access_Modifier.Internal
+{
+    internal class MyClass
+    {
+        internal int a = 10;
+        public void MyMethod()
+        {
+            Console.WriteLine("value of a is " + a);
+        }
+    }
+}
