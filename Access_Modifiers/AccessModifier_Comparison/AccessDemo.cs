bc9f6ef64ecf981b90dd7a0b78f4f0090e3106d2
Ragini340 <ragini31bxr@gmail.com>
2026-06-07 16:05:17 +0530

    #Practice Added Access Modifier Real Time Example & Comparison Example

---
+++ Access_Modifiers/AccessModifier_Comparison/AccessDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Access_Modifiers.AccessModifier_Comparison
+{
+    public class AccessDemo
+    {
+        public int PublicValue = 10;
+        private int PrivateValue = 20;
+        protected int ProtectedValue = 30;
+        internal int InternalValue = 40;
+
+        public void Display()
+        {
+            Console.WriteLine("Public Value    : " + PublicValue);
+            Console.WriteLine("Private Value   : " + PrivateValue);
+            Console.WriteLine("Protected Value : " + ProtectedValue);
+            Console.WriteLine("Internal Value  : " + InternalValue);
+        }
+
+        public static void Main(string[] args)
+        {
+            AccessDemo obj = new AccessDemo();
+            obj.Display();
+        }
+
+    }
+}
---
+++ Access_Modifiers/RealTime_Example/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Access_Modifiers.RealTime_Example
+{
+    public class BankAccount
+    {
+        private double balance;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void DisplayBalance()
+        {
+            Console.WriteLine("Current Balance : " + balance);
+        }
+
+        public static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+
+            account.Deposit(5000);
+
+            account.DisplayBalance();
+        }
+
+    }
+}
