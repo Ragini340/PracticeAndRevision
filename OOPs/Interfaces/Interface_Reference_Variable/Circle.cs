bd3ad6ecfe3fd9a722ed0a6492ff7fb2bc8d13a3
Ragini340 <ragini31bxr@gmail.com>
2026-06-10 09:21:57 +0530

    #Practice Interface Reference Variable POC

---
+++ OOPs/Interfaces/Interface_Reference_Variable/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.OOPs.Interfaces.Interface_Reference_Variable
+{
+    public class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Circle");
+        }
+    }
+}
---
+++ OOPs/Interfaces/Interface_Reference_Variable/Driver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.OOPs.Interfaces.Interface_Reference_Variable
+{
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            IShape shape = new Circle();
+
+            shape.Draw();
+        }
+    }
+}
