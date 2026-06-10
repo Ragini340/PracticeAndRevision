51b9fdbb65f45bd2cb2d8f58ab1f4c0b9ade9aef
Ragini340 <ragini31bxr@gmail.com>
2026-06-10 09:15:10 +0530

    #Practice Implemented Interface Real Time Example

---
+++ OOPs/Interfaces/RealTime_Example/Driver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.OOPs.Interfaces.RealTime_Example
+{
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            ILogger logger = new FileLogger();
+
+            logger.Log("Application Started");
+        }
+    }
+}
---
+++ OOPs/Interfaces/RealTime_Example/FileLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.OOPs.Interfaces.RealTime_Example
+{
+    public class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("File Log : " + message);
+        }
+    }
+}
