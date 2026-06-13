c3862c96c9173016d2ae137499fc9ea5f0c63e60
Ragini340 <ragini31bxr@gmail.com>
2026-06-13 14:06:26 +0530

    #Practice Generic Stack and Queue POC

---
+++ Collections/Queues/Generic_Queue/QueueDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Collections.Queues.Generic_Queue
+{
+    public class QueueDemo
+    {
+        public void Execute()
+        {
+            Queue<int> numbers = new Queue<int>();
+
+            numbers.Enqueue(10);
+            numbers.Enqueue(20);
+            numbers.Enqueue(30);
+
+            while (numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Dequeue());
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            QueueDemo demo = new QueueDemo();
+            demo.Execute();
+        }
+    }
+}
---
+++ Collections/Stacks/Generic_Stack/StackDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Collections.Stacks.Generic_Stack
+{
+    public class StackDemo
+    {
+        public void Execute()
+        {
+            Stack<int> numbers = new Stack<int>();
+
+            numbers.Push(10);
+            numbers.Push(20);
+            numbers.Push(30);
+
+            while (numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Pop());
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            StackDemo demo = new StackDemo();
+            demo.Execute();
+        }
+    }
+}
