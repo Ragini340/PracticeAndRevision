fdf687b16285e7fc5b2b56358317f384f5a6c859
Ragini340 <ragini31bxr@gmail.com>
2026-06-22 10:30:00 +0530

    #Practice Extension Method POC

---
+++ Extension_Methods/Simple_Extension_Method/ExtensionClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndRevision.Extension_Methods.Simple_Extension_Method
+{
+    public static class ExtensionClass
+    {
+        public static void ShowMessage(this string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Main(string[] args)
+        {
+            string text = "Hello Extension Method";
+
+            text.ShowMessage();
+        }
+    }
+}
